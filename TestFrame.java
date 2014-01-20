15b0db2dbe2a37e649da948f0f45ce36d3591d5c
andrewguo5 <andrew.guo5@gmail.com>
2014-01-19 23:04:52 -0500

    Further GUI exploration. Buttons now can print statements into the console

--- TestFrame.java
+++ TestFrame.java
@@ -7,16 +7,23 @@ public class TestFrame extends JFrame implements ActionListener{
     JLabel answer = new JLabel("");
     JPanel pane = new JPanel();
     JButton buttonlol = new JButton("click this button lol");
+    JButton anotherbuttonlol = new JButton("no click this button lol");
     public TestFrame() {
 	super("TestFrame Swag Oldmoney"); 
 	setBounds(300, 200, 300, 400);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	Container con = this.getContentPane();
 	con.add(pane);
+	con.setBackground(Color.ORANGE);
+	anotherbuttonlol.setMnemonic('o');
+	anotherbuttonlol.addActionListener(this);
 	buttonlol.setMnemonic('k');
 	buttonlol.addActionListener(this);
-	pane.add(answer);
+	/////con.add(buttonlol);
+	//pane.add(answer);
 	pane.add(buttonlol);
+	pane.add(anotherbuttonlol);
+	//pane.setBackground(Color.ORANGE);
 	//buttonlol.requestFocus();?
 	setVisible(true);
     }
@@ -24,8 +31,13 @@ public class TestFrame extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent bang) {
 	Object source = bang.getSource();
 	if (source == buttonlol) {
-	    answer.setText("This text magically appeared.");
-	    JOptionPane.showMessageDialog(null, "You just clicked the button", "Congrats", JOptionPane.PLAIN_MESSAGE);
+	    System.out.println("LEL");
+	    //answer.setText("This text magically appeared.");
+	    //JOptionPane.showMessageDialog(null, "You just clicked the button", "Congrats", JOptionPane.PLAIN_MESSAGE);
+	    setVisible(true);
+	}
+	if (source == anotherbuttonlol) {
+	    System.out.println("TOPLEL");
 	    setVisible(true);
 	}
     }
