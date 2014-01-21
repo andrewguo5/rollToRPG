58cdd6c598ae10f80a99dfcdc8221c2f50e963a1
andrewguo5 <andrew.guo5@gmail.com>
2014-01-21 13:12:09 -0500

    Driver file gainst experience

--- Map/Driver.java
+++ Map/Driver.java
@@ -1,6 +1,11 @@
 import cs1.Keyboard;
 
 public class Driver {
+
+    public static boolean islegitDirection(String d) {
+	return( d.equals("north") || d.equals("south") || d.equals("west") || d.equals("east") || d.equals("northwest") || d.equals("northeast") || d.equals("southeast") || d.equals("southwest")|| d.equals("south west") || d.equals("south east") || d.equals("north west") || d.equals("north east") );
+    }
+
     public static void main (String[] args) {
 	System.out.print("Enter the name of player1: ");
 	String name1 = Keyboard.readString();
@@ -23,15 +28,39 @@ public class Driver {
 	    for (Player fred: map.getplayerList()) {
 		Player temp = fred;
 		System.out.println("Turn begin");
-		GuiStuff.setup(temp);
+		System.out.println("\n\n");
+		System.out.println( map.getTileMap()[temp.getXcor()][temp.getYcor()].closeUp() ); //examine
+		while (temp.getActionsLeft() > 0) {
+		    System.out.println("\n\n\nWhat do you want to do? (Actions left: " + temp.getActionsLeft() + " )" );
+		    System.out.println("'look'- Look at a neighboring tile (Costs 1 move)");
+		    System.out.println("'harvest'- Search for bonuses on current tile (Costs 3 moves)");
+		    System.out.println("'attack'- Swing your sword wildly (Costs 2 moves)");
+		    System.out.println("'move'- Run blindly into a neighboring tile (Costs 1 move)");
+		    System.out.println("'help'- Open help panel");
+		    System.out.println("'map'- Open map");
+		    String response = Keyboard.readString();
+		    if (response.equals("look") ) {
+			String direction = "";
+			System.out.println("What direction do you want to look in?");
+			direction = (Keyboard.readString()).toLowerCase();
+			while ( !islegitDirection(direction) ) {
+			    System.out.println("I don't know what direction " + direction + " is in.");
+			    System.out.println("What direction do you want to look in?");
+			    direction = (Keyboard.readString()).toLowerCase();
+			}
+			temp.setLooking(true, direction);
+			System.out.println("\n\n");
+			map.updatePlayer(temp);
+			temp.setActionsLeft(temp.getActionsLeft() - 1 );
+		    }
+		}
+		    
+			
 		String stop = Keyboard.readString();
-		//System.out.println( map.getTileMap()[fred.getXcor()][fred.getYcor()].closeUp() );
-		
+	
 		
 	        
 	    }
 	}
-
-
     }
 }
