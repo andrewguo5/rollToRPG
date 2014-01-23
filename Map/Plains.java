b78f87d80db32bca687e0d19ea2807ffc2c1ff4a
andrewguo5 <andrew.guo5@gmail.com>
2014-01-23 14:21:58 -0500

    Modified item drop %

--- Map/Plains.java
+++ Map/Plains.java
@@ -5,7 +5,7 @@ public class Plains extends Tile {
 
     public Plains() {
 	super();
-	if ( (3* Math.random()) > 1 ) 
+	if ( (5* Math.random()) > 1 ) 
 	    _item = new Item("rough grass (+10 con, +5 fth).",0,0,0,5,0,10,0,0);
 	else 
 	    _item = new Item("buffalo horn filled with eucalyptus juice (+20 con, +5 fth).",0,0,0,5,0,20,0,0);
